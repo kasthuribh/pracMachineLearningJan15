09839fd8e0710571abea146d374139435b8af48b
kasthuribh <kasthuribh@gmail.com>
2015-01-15 18:34:53 +0530

    completed up to week 2 lect 1

--- lect/caret_demo.R
+++ lect/caret_demo.R
@@ -1,4 +1,13 @@
 library(caret); library(kernlab);data(spam)
 inTrain<-createDataPartition(y = spam$type,p = 0.75, list = FALSE)
-training<-spam[inTrain,]
-test<-spam[-inTrain,]
+trainSet<-spam[inTrain,]
+testSet<-spam[-inTrain,]
+dim(trainSet)
+set.seed(1234)
+modelFit<-train(type~.,data = trainSet, method="glm")
+modelFit
+modelFit$finalModel
+#spam[1,]
+predictions<-predict(modelFit,newdata = testSet)
+predictions
+confusionMatrix(predictions,testSet$type)
