1d84fdf4be397c330dab4446e4773c21752f67b1
kasthuribh <kasthuribh@gmail.com>
2015-01-15 19:10:24 +0530

    in the middle of wek2 lect2

--- lect/caret_demo.R
+++ lect/caret_demo.R
@@ -1,3 +1,4 @@
+#week 2 lect 1
 library(caret); library(kernlab);data(spam)
 inTrain<-createDataPartition(y = spam$type,p = 0.75, list = FALSE)
 trainSet<-spam[inTrain,]
@@ -11,3 +12,20 @@ modelFit$finalModel
 predictions<-predict(modelFit,newdata = testSet)
 predictions
 confusionMatrix(predictions,testSet$type)
+
+#week 2 lect 2
+
+#cross validation
+set.seed(32323)
+folds=createFolds(spam$type,k = 10, list = TRUE, returnTrain = TRUE)
+sapply(folds,length)
+folds[[1]][1:10]
+
+set.seed(32323)
+folds=createFolds(spam$type,k = 10, list = TRUE, returnTrain = FALSE)
+sapply(folds,length)
+
+set.seed(32323)
+folds=createResample(y = spam$type, times = 10, list = TRUE)
+sapply(folds,length)
+folds[[1]][1:10]
